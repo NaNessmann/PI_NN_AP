645fb8f70a276acea45e2f5004885bf15c7f40bc
NaNessmann <nessmannarq@gmail.com>
2023-05-15 21:30:18 -0300

    changes

--- PI_NN_AP/Frontend/src/app/service/s-experiencia.service.ts
+++ PI_NN_AP/Frontend/src/app/service/s-experiencia.service.ts
@@ -8,7 +8,8 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-  URL = 'http://localhost:8090/explab/';
+
+  URL = environment.URL + 'explab/';
 
   constructor(private httpClient: HttpClient) { }
 
